1c3721cf17bf8b482c6f260b017fc86a3f1b07a0
Blake Williams <blake@blakewilliams.me>
2020-08-12 23:45:47 -0400

    Add basic test

--- redgreen/test/redGreenTest.ts
+++ redgreen/test/redGreenTest.ts
@@ -1,8 +1,18 @@
 import assert from "../lib/assert"
 import RedGreen, { Runner } from "../lib/redgreen"
 
-RedGreen.test("hello world", () => {
-  assert(false)
+RedGreen.test("hello world", async () => {
+  const runner = new Runner()
+  runner.reporter = undefined
+
+  let called = false
+
+  runner.test("fake", () => {
+    called = true
+  })
+
+  await runner.run()
+  assert(called, "Expected called to be truthy")
 })
 
 RedGreen.test("hello world", () => {
