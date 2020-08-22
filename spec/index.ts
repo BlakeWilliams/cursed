1f0f880cb417a3a7032e5dce6533a6726e5ec8ce
Blake Williams <blake@blakewilliams.me>
2020-08-22 16:32:06 -0400

    Export `createCursedCommands`
    `package.json` exports don't work in TypeScript yet, so this is the
    workaround.

--- spec/index.ts
+++ spec/index.ts
@@ -2,9 +2,10 @@ import Runner from "./lib/runner";
 import Reporter, { Reportable } from "./lib/reporter";
 import Test from "./lib/test";
 import type Context from "./lib/context";
+import createCursedCommands from "./lib/createCursedCommands"
 import assert from "./lib/assert";
 
-export { assert, Runner, Reporter, Reportable, Test };
+export { assert, createCursedCommands, Runner, Reporter, Reportable, Test };
 export type { Context };
 
 export default new Runner();
